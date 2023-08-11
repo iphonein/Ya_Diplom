3c13572c506d2e33dfdc0a45bdaf81a42b11f018
Evgenii Afonin <afonin.e.m@gmail.com>
2023-08-11 04:38:10 +0400

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# Афонин Евгений, 7-я когорта - Финальный проект. Инженер по тестированию плюс
+
+URL_SERVICE = "https://1807d65b-96a9-42e9-a35e-66ffd06a3f4f.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,15 @@
+# Афонин Евгений, 7-я когорта - Финальный проект. Инженер по тестированию плюс
+
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+78003553535",
+    "rentTime": 5,
+    "deliveryDate": "2023-08-30",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
